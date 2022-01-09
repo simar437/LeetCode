52556b91259929bcba80b6314a181e07332df65f
simar437 <58173697+simar437@users.noreply.github.com>
2022-01-09 20:46:46 +0530

    Time: 181 ms (18.94%), Space: 47.4 MB (65.34%) - LeetHub

---
+++ 398-random-pick-index/398-random-pick-index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    unordered_multimap<int, int> m;
+    random_device rd{};
+public:
+    Solution(const vector<int>& nums) {
+        for (int x{}; const auto& i : nums)
+        {
+            m.insert(begin(m), {i, x++});
+        }
+    }
+    
+    int pick(int target) {
+        auto [begin, end] = m.equal_range(target);
+        uniform_int_distribution<int> dist(0, distance(begin, end) - 1);
+        return next(begin, dist(rd))->second;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
