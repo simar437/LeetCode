cb01cea7f5e3fce38e62961c5f00d2460aa3be3c
simar437 <58173697+simar437@users.noreply.github.com>
2022-01-22 17:11:21 +0530

    Time: 51 ms (62.42%), Space: 11 MB (40.27%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -2,11 +2,12 @@ class Solution {
     unordered_map<int, bool> m;
 public:
     bool winnerSquareGame(int n) {
-        if (auto x = sqrt(n); x == (int)x)
+        auto x = sqrt(n);
+        if (x == (int)x)
             return true;
         if (auto it = m.find(n); it != end(m))
             return it->second;
-        for (int i{1}; i * i <= n; i++)
+        for (int i = x; i > 0; i--)
         {
             if (!winnerSquareGame(n - i * i))
             {
