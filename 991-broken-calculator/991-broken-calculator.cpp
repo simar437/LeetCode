b17a38e8a69fe861b2ce21006054037418273589
simar437 <58173697+simar437@users.noreply.github.com>
2022-02-25 12:54:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.50%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        if (startValue > target)
+        if (startValue >= target)
             return startValue - target;
-        if (startValue == target)
-            return 0;
         if (target % 2 == 0)
             target /= 2;
         else
