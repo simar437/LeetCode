c6a32948cd315933e1eaca56900cb4e48b95d330
simar437 <58173697+simar437@users.noreply.github.com>
2022-03-16 17:04:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (9.47%) - LeetHub

--- 1556-thousand-separator/1556-thousand-separator.cpp
+++ 1556-thousand-separator/1556-thousand-separator.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     string thousandSeparator(int n) {
         string num = to_string(n);;
-        for (int i = size(num) - 1, x{}; i >= 0; i--) {
+        for (int i = size(num) - 1, x{}; i >= 0; i--, x++) {
             if (x % 3 == 0 && x != 0) {
                 num.insert(begin(num) + i + 1, '.');
             }
-            x++;
         }
         return num;
     }
