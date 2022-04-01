9127fefac3d9ebb1785f95af762d06978ef61140
simar437 <58173697+simar437@users.noreply.github.com>
2022-04-01 16:43:34 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (89.72%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -2,6 +2,6 @@ class Solution {
     array<int, 5> arr{6, 28, 496, 8128, 33550336};
 public:
     bool checkPerfectNumber(int num) {
-        return find(begin(arr), end(arr), num) != end(arr);
+        return binary_search(begin(arr), end(arr), num);
     }
 };
