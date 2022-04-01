d66a7f3b908a5ed3ad6c34493c5eaa19b8025b03
simar437 <58173697+simar437@users.noreply.github.com>
2022-04-01 16:43:09 +0530

    Time: 5 ms (60.24%), Space: 5.9 MB (22.01%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -1,14 +1,7 @@
 class Solution {
+    array<int, 5> arr{6, 28, 496, 8128, 33550336};
 public:
     bool checkPerfectNumber(int num) {
-        int sum{1}, temp{num};
-        for (int i{2}; i < temp; i++)
-        {
-            if (num % i == 0) {
-                temp = num / i;
-                sum += i + temp;
-            }
-        }
-        return sum == num && num != 1;
+        return find(begin(arr), end(arr), num) != end(arr);
     }
 };
