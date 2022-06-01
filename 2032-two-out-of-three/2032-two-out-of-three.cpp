42bd927df90f855915f6f6de44ad4467aec61bb2
simar437 <58173697+simar437@users.noreply.github.com>
2022-06-01 13:15:56 +0530

    Time: 29 ms (56.26%), Space: 28 MB (47.47%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.cpp
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -1,13 +1,15 @@
 class Solution {
     unordered_map<int, int> m;
-    void helper(vector<int>& nums) {
+    void helper(const vector<int>& nums) {
         unordered_set<int> s(begin(nums), end(nums));
         for (const auto& i : s) {
             m[i]++;
         }
     }
 public:
-    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+    vector<int> twoOutOfThree(const vector<int>& nums1,
+                              const vector<int>& nums2,
+                              const vector<int>& nums3) {
         helper(nums1);
         helper(nums2);
         helper(nums3);
